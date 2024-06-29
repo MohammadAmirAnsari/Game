623dacbaa84f5d2ed8fb6a1733d3eb732bda1cb8
Mohammad_Amir <aamiransari92111721@gmail.com>
2024-06-29 20:05:25 +0530

    first commit

---
+++ try/nsg.js
@@ -0,0 +1,24 @@
+function handleSubmit(event) {
+    root = document.getElementById("root");
+    if (document.getElementsByClassName("grid").length > 0)
+        document.getElementsByClassName("grid")[0].remove();
+    // console.log('Submit button clicked');
+    event.preventDefault();
+    rows = document.getElementById("rows").value;
+    columns = document.getElementById("columns").value;
+    div = document.createElement("div");
+    div.classList.add("grid");
+    div.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+    div.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+    root.appendChild(div);
+    let count = 1
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            let item = document.createElement("div");
+            item.classList.add("grid-items");
+            item.innerHTML = count++;
+            div.appendChild(item);
+        }
+    }
+}
+
