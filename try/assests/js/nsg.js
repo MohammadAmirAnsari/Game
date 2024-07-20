1a30a3bf866cbc0823ad5d03fa3de94f63eb36d2
Mohammad Amir <aamiransari92111721@gmail.com>
2024-07-20 22:47:38 +0530

    update w.r.t sass

--- try/assests/js/nsg.js
+++ try/assests/js/nsg.js
@@ -1,4 +1,5 @@
 function handleSubmit(event) {
+
     root = document.getElementById("div-root");
     if (document.getElementsByClassName("grid").length > 0)
         document.getElementsByClassName("grid")[0].remove();
@@ -10,16 +11,36 @@ function handleSubmit(event) {
     div.classList.add("grid");
     div.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
     div.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-    div.style.gap = "1rem 1rem";
+    div.style.gap = "2.5rem 2.5rem";
     root.appendChild(div);
     let count = 1
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < columns; j++) {
             let item = document.createElement("div");
             item.classList.add("grid-items");
-            item.innerHTML = count++;
+            if (count === rows * columns) {
+                item.innerHTML = "MOVE";
+            } else {
+                item.innerHTML = count++;
+
+            }
             div.appendChild(item);
         }
     }
+    div.scrollIntoView({ block: "center", behavior: "smooth" });
+    lastChild = document.querySelectorAll('.grid-items:last-child')[0];
+    lastChild.classList.add('shifter');
+
 }
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'ArrowUp' || event.key === 'ArrowDown' || event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
+        event.preventDefault();
+        lastChild = document.querySelector('.shifter')
+        sixthChild = document.querySelectorAll('.grid-items')[5]
+        temp = lastChild.innerHTML
+        lastChild.innerHTML = sixthChild.innerHTML
+        sixthChild.innerHTML = temp
+    }
+},)
 
